2e1095ebb15b96ed480623ec7dae299d842a284b
Дмитрий Астахов <teamvortex@yandex.ru>
2020-02-24 16:01:33 +0300

    final6

--- include/header.hpp
+++ include/header.hpp
@@ -10,7 +10,6 @@
 #include <boost/log/trivial.hpp>
 #include <random>
 #include <boost/log/core.hpp>
-#include <boost/log/trivial.hpp>
 #include <boost/log/expressions.hpp>
 #include <boost/log/utility/setup/file.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
